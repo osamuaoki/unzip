d66e5e6e834a110c8793d6c4be2703d90c543c44
Santiago Vila <sanvila@debian.org>
2005-04-24 19:27:02 +0200

    Import Debian changes 5.52-2
    unzip (5.52-2) unstable; urgency=low
    .
      * Fixed typos in manpage (Closes: #301915).

--- unzpriv.h
+++ unzpriv.h
@@ -1081,7 +1081,6 @@
 #  define FOPR  "r","ctx=stm"
 #  define FOPM  "r+","ctx=stm","rfm=fix","mrs=512"
 #  define FOPW  "w","ctx=stm","rfm=fix","mrs=512"
-#  define FOPWR "w+","ctx=stm","rfm=fix","mrs=512"
 #endif /* VMS */
 
 #ifdef CMS_MVS
@@ -1118,9 +1117,6 @@
 #  ifndef FOPWT
 #    define FOPWT "wt"
 #  endif
-#  ifndef FOPWR
-#    define FOPWR "w+b"
-#  endif
 #else /* !MODERN */
 #  ifndef FOPR
 #    define FOPR "r"
@@ -1133,9 +1129,6 @@
 #  endif
 #  ifndef FOPWT
 #    define FOPWT "w"
-#  endif
-#  ifndef FOPWR
-#    define FOPWR "w+b"
 #  endif
 #endif /* ?MODERN */
 
