f0171043233e59b04eafb1fa154d163acb91496c
Santiago Vila <sanvila@debian.org>
2005-11-17 16:34:24 +0100

    Import Debian changes 5.52-5
    unzip (5.52-5) unstable; urgency=low
    .
      * Fixed CAN-2005-2475 the same way it will be fixed in unzip 5.53.
        Patch extracted from a prerelease provided by upstream.
      * Changed unzip banner line to reflect the fact that this is
        a "modified" release. Debian-derived distributions should probably
        do the same if they deviate from the Debian version.

--- unix/unix.c
+++ unix/unix.c
@@ -1040,7 +1040,7 @@ void close_outfile(__G)    /* GRR: change to return PK-style warning level */
         ztimbuf t2;             /* modtime, actime */
     } zt;
     ush z_uidgid[2];
-    int have_uidgid_flg, outfd;
+    int have_uidgid_flg;
 
 /*---------------------------------------------------------------------------
     If symbolic links are supported, allocate storage for a symlink control
@@ -1061,6 +1061,7 @@ void close_outfile(__G)    /* GRR: change to return PK-style warning level */
             Info(slide, 0x201, ((char *)slide,
               "warning:  symbolic link (%s) failed: mem alloc overflow\n",
               FnFilter1(G.filename)));
+            fclose(G.outfile);
             return;
         }
 
@@ -1068,6 +1069,7 @@ void close_outfile(__G)    /* GRR: change to return PK-style warning level */
             Info(slide, 0x201, ((char *)slide,
               "warning:  symbolic link (%s) failed: no mem\n",
               FnFilter1(G.filename)));
+            fclose(G.outfile);
             return;
         }
         slnk_entry->next = NULL;
@@ -1077,11 +1079,10 @@ void close_outfile(__G)    /* GRR: change to return PK-style warning level */
         slnk_entry->fname = slnk_entry->target + ucsize + 1;
         strcpy(slnk_entry->fname, G.filename);
 
-        /* reopen the "link data" file for reading */
-        G.outfile = freopen(G.filename, FOPR, G.outfile);
+        /* move back to the start of the file to re-read the "link data" */
+        rewind(G.outfile);
 
-        if (!G.outfile ||
-            fread(slnk_entry->target, 1, ucsize, G.outfile) != (int)ucsize)
+        if (fread(slnk_entry->target, 1, ucsize, G.outfile) != (int)ucsize)
         {
             Info(slide, 0x201, ((char *)slide,
               "warning:  symbolic link (%s) failed\n",
@@ -1113,13 +1114,20 @@ void close_outfile(__G)    /* GRR: change to return PK-style warning level */
     }
 #endif
 
+#if (defined(NO_FCHOWN) || defined(NO_FCHMOD))
+    fclose(G.outfile);
+#endif
+
     have_uidgid_flg = get_extattribs(__G__ &(zt.t3), z_uidgid);
-    outfd = fileno(G.outfile);
 
     /* if -X option was specified and we have UID/GID info, restore it */
     if (have_uidgid_flg) {
         TTrace((stderr, "close_outfile:  restoring Unix UID/GID info\n"));
-        if (fchown(outfd, (uid_t)z_uidgid[0], (gid_t)z_uidgid[1]))
+#if (defined(NO_FCHOWN) || defined(NO_FCHMOD))
+        if (chown(G.filename, (uid_t)z_uidgid[0], (gid_t)z_uidgid[1]))
+#else
+        if (fchown(fileno(G.outfile), (uid_t)z_uidgid[0], (gid_t)z_uidgid[1]))
+#endif
         {
             if (uO.qflag)
                 Info(slide, 0x201, ((char *)slide,
@@ -1131,6 +1139,18 @@ void close_outfile(__G)    /* GRR: change to return PK-style warning level */
                   z_uidgid[0], z_uidgid[1]));
         }
     }
+
+#if (!defined(NO_FCHOWN) && !defined(NO_FCHMOD))
+/*---------------------------------------------------------------------------
+    Change the file permissions from default ones to those stored in the
+    zipfile.
+  ---------------------------------------------------------------------------*/
+
+    if (fchmod(fileno(G.outfile), filtattr(__G__ G.pInfo->file_attr)))
+        perror("chmod (file attributes) error");
+
+    fclose(G.outfile);
+#endif /* !NO_FCHOWN && !NO_FCHMOD */
 
     /* set the file's access and modification times */
     if (utime(G.filename, &(zt.t2))) {
@@ -1150,17 +1170,17 @@ void close_outfile(__G)    /* GRR: change to return PK-style warning level */
 #endif /* ?AOS_VS */
     }
 
+#if (defined(NO_FCHOWN) || defined(NO_FCHMOD))
 /*---------------------------------------------------------------------------
     Change the file permissions from default ones to those stored in the
     zipfile.
   ---------------------------------------------------------------------------*/
 
 #ifndef NO_CHMOD
-    if (fchmod(outfd, filtattr(__G__ G.pInfo->file_attr)))
+    if (chmod(G.filename, filtattr(__G__ G.pInfo->file_attr)))
         perror("chmod (file attributes) error");
 #endif
-
-    fclose(G.outfile);
+#endif /* NO_FCHOWN || NO_FCHMOD */
 
 } /* end function close_outfile() */
 
@@ -1641,7 +1661,6 @@ static void qlfix(__G__ ef_ptr, ef_len)
 
             if ((long)LG(dlen) > 0)
             {
-                G.outfile = fopen(G.filename,"r+");
                 fseek(G.outfile, -8, SEEK_END);
                 fread(&ntc, 8, 1, G.outfile);
                 if(ntc.id != *(long *)"XTcc")
@@ -1651,7 +1670,6 @@ static void qlfix(__G__ ef_ptr, ef_len)
                     fwrite (&ntc, 8, 1, G.outfile);
                 }
                 Info(slide, 0x201, ((char *)slide, "QData = %d", LG(dlen)));
-                fclose(G.outfile);
             }
             return;     /* finished, cancel further extra field scanning */
           }
